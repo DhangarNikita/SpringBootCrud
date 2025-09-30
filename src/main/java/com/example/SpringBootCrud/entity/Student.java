cfa9438a009ddd7b8a6b98d4415d812b7c76f401
DhangarNikita <dhangarnikita13@gmail.com>
2025-09-30 15:10:31 +0530

    crud

---
+++ src/main/java/com/example/SpringBootCrud/entity/Student.java
@@ -0,0 +1,21 @@
+package com.example.SpringBootCrud.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "student_list")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int rollNo;
+    String name;
+    float percentage;
+    String branch;
+}
---
+++ src/main/java/com/example/SpringBootCrud/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.example.SpringBootCrud.repository;
+
+import com.example.SpringBootCrud.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+}
